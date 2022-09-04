73971c2888119a0df6ebfbf4912554baafc5f366
yun <yunzheng2@unm.edu>
2022-09-03 20:42:35 -0600

    need to work on prevent the same tile compare to itself.

--- src/com/yun/unm/titlegame/Tile.java
+++ src/com/yun/unm/titlegame/Tile.java
@@ -1,17 +1,114 @@
 package com.yun.unm.titlegame;
 
-public class Tile {
-    // add a comment
+import javafx.event.EventHandler;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
+import javafx.scene.shape.TriangleMesh;
 
-   int color;
-   int shape;
-   int size;
+import java.util.Random;
 
-    public Tile(int color, int shape, int size) {
-        this.color = color;
-        this.shape = shape;
-        this.size = size;
+public class Tile extends StackPane {
+    private static Random random =new Random();
+    private static Tile firstTile=null;
+    private Color shape1Color;
+    private Color shape2Color;
+    private Color shape3Color;
+    private Shape shape1;
+    private Shape shape2;
+    private Shape shape3;
+    int currentStreak = 0;
+    int longestStreak = 0;
+
+
+    public Tile(){
+        shape1Color=getColor();
+        do {
+            shape2Color = getColor();
+        }while(shape1Color.equals(shape2Color));
+        do{
+            shape3Color=getColor();
+        }while(shape2Color.equals(shape3Color));
+
+        shape1 = getShape(shape1Color, 1);
+        shape2 = getShape(shape2Color, 2);
+        shape3 = getShape(shape3Color, 3);
+        getChildren().addAll(shape3, shape2, shape1);
+        setOnMouseClicked(new EventHandler<MouseEvent>(){
+            @Override
+            public void handle(MouseEvent event){
+                if (firstTile==null){
+                    firstTile=(Tile)event.getSource() ;
+
+                }else{
+                    Tile secondTile = (Tile)event.getSource();
+                    if(firstTile.compareTiles(secondTile)){
+                        System.out.println("two tile are same");
+                    }else{
+                        System.out.println("two tile are different");
+                    }
+                    firstTile=null;
+                }
+            }
+        });
     }
-    int 
+    public boolean compareTiles(Tile tile){
 
+        boolean tileMatches = false;
+        if(shape1Color.equals(tile.shape1Color) && shape1.equals(tile.shape1)){
+            getChildren().remove(shape1);
+            tile.getChildren().remove(tile.shape1);
+            System.out.println("shape1 matches");
+            tileMatches=true;
+        }
+        if(shape2Color.equals(tile.shape2Color) && shape2.equals(tile.shape2)){
+            getChildren().remove(shape2);
+            tile.getChildren().remove(tile.shape2);
+            System.out.println("shape2 matches");
+            tileMatches=true;
+        }
+        if(shape3Color.equals(tile.shape3Color) && shape3.equals(tile.shape3)){
+            getChildren().remove(shape3);
+            tile.getChildren().remove(tile.shape3);
+            System.out.println("shape3 matches");
+            tileMatches=true;
+        }
+        if(tileMatches==true){
+            currentStreak+=1;
+            longestStreak+=1;
+        }else{
+            currentStreak=0;
+        }
+        return tileMatches;
+    }
+    private static Color getColor(){
+        int rand= random.nextInt(5);
+        return switch (rand) {
+            case 0 -> Color.RED;
+            case 1 -> Color.PURPLE;
+            case 2 -> Color.GREEN;
+            case 3 -> Color.BLUE;
+            case 4 -> Color.YELLOW;
+            default -> Color.BLACK;
+        };
+    }
+    private static Shape getShape(Color color, int order){
+        int size=10;
+        if(order==2){
+            size=30;
+        }else if(order==3){
+            size=60;
+        }
+        int rand = random.nextInt(2);
+        return switch(rand) {
+            case 0-> new Rectangle(size,size, color);
+            case 1-> new Circle(size, color);
+            default -> null;
+        };
+
+    }
 }
---
+++ src/sample/CanvasDemo.java
@@ -0,0 +1,41 @@
+package sample;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.TilePane;
+import javafx.stage.Stage;
+
+public class MainView2 extends Application {
+    @Override
+    public void start(Stage stage) {
+        //Creating the tile pane
+        TilePane tilePane = new TilePane();
+        StackPane stackPane = null;
+
+        //Setting the orientation for the Tile Pane
+        tilePane.setOrientation(Orientation.VERTICAL);
+        //Setting the alignment for the Tile Pane
+        tilePane.setTileAlignment(Pos.CENTER);
+        //Setting the preferred columns for the Tile Pane
+        tilePane.setPrefColumns(6); //why I setting preferred row or columns do not affect?
+        // set Horizontal Gap and Vertical Gap
+        tilePane.setHgap(5);
+        tilePane.setVgap(5);
+
+        for (int i = 0; i < 9; i++) {
+            Tile tile =new Tile();
+            tilePane.getChildren().add(tile);
+        }
+
+        Scene scene = new Scene(tilePane, 730, 730);
+        stage.setTitle("stackPane inside tilePane");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String args[]) {
+        launch(args);
+    }
+}
